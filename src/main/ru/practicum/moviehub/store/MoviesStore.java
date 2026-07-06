30cf81887348fb353fdde2107b2a17206b49e4e1
Michael <kinovar13@gmail.com>
2026-07-07 04:51:00 +0700

    Implement MovieHub API

--- src/main/ru/practicum/moviehub/store/MoviesStore.java
+++ src/main/ru/practicum/moviehub/store/MoviesStore.java
@@ -1,4 +1,42 @@
 package ru.practicum.moviehub.store;
 
+import ru.practicum.moviehub.model.Movie;
+
+import java.util.*;
+
 public class MoviesStore {
+    private final Map<Integer, Movie> movies = new HashMap<>();
+    private int nextId = 1;
+
+    public List<Movie> getAllMovies() {
+        return new ArrayList<>(movies.values());
+    }
+
+    public Optional<Movie> getMovieById(int id) {
+        return Optional.ofNullable(movies.get(id));
+    }
+
+    public List<Movie> getMoviesByYear(int year) {
+        return movies.values()
+                .stream()
+                .filter(movie -> movie.getYear() == year)
+                .toList();
+    }
+
+    public Movie add(Movie movie) {
+        movie.setId(nextId);
+        nextId++;
+        movies.put(movie.getId(), movie);
+        return movie;
+    }
+
+    public boolean delete(int id) {
+        return movies.remove(id) != null;
+    }
+
+    public void clear() {
+        movies.clear();
+        nextId = 1;
+    }
 }
+
